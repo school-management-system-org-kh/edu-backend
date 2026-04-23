0742d5163f9ed0652cc1ebb762853534db24ef87
Chham Dararaksmey <58686050+chhamgit@users.noreply.github.com>
2026-04-23 09:52:49 +0700

    Fixed wrong url

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 // export const ADMIN_ENDPOINT = "http://localhost:5000/api/v1";
 
 // Staging
-export const ADMIN_ENDPOINT = "https://edu-api-vzh6.onrender.com";
+export const ADMIN_ENDPOINT = "https://edu-api-vzh6.onrender.com/api/v1";
 
 export default {
   ADMIN_ENDPOINT,
